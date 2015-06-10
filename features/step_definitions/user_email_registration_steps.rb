8521bbdbcf6cf7ca24278dcc8c608cd763809659
Laurie Williams <laurie.huw.williams@googlemail.com>
2015-06-10 10:20:30 +0100

    Add user_email_registration.feature step definitions

--- features/step_definitions/user_email_registration_steps.rb
+++ features/step_definitions/user_email_registration_steps.rb
@@ -1,51 +1,54 @@
 Given(/^I visit the User Registration page$/) do
-  pending # express the regexp above with the code you wish you had
+  visit(new_user_registration_path)
 end
 
 And(/^I fill in the User Registration form correctly$/) do
-  pending # express the regexp above with the code you wish you had
+  fill_in("Name", with: "John")
+  fill_in("Username", with: "John89")
+  fill_in("Email", with: "John89@example.com")
+  fill_in("Password", with: "secretpassword")
 end
 
 And(/^I click Register$/) do
-  pending # express the regexp above with the code you wish you had
+  click_button("Create Account")
 end
 
 Then(/^I am redirected to the User Edit page$/) do
-  pending # express the regexp above with the code you wish you had
+  expect(current_path).to eq(edit_user_path(1))
 end
 
 And(/^the Username field contains correct Username$/) do
-  pending # express the regexp above with the code you wish you had
+  withing("#form"){expect(page).to have_field("Name", with: "John")}
 end
 
 When(/^I click my User Profile Link$/) do
-  pending # express the regexp above with the code you wish you had
+  click_link("John")
 end
 
 Then(/^I am redirected to my User Profile page$/) do
-  pending # express the regexp above with the code you wish you had
+  expect(current_path).to eq(user_path(1))
 end
 
 And(/^I can see my Username on the page$/) do
-  pending # express the regexp above with the code you wish you had
+  within("#main"){ expect(page).to have_content("John")}
 end
 
 And(/^I can see Sign Out Link$/) do
-  pending # express the regexp above with the code you wish you had
+  expect(page).to have_link("Sign Out", destroy_user_session_path )
 end
 
 And(/^I fill in the User Registration form incorrectly$/) do
-  pending # express the regexp above with the code you wish you had
+  fill_in("Name", with: "John")
 end
 
 Then(/^I am redirected back to the User Registration Page$/) do
-  pending # express the regexp above with the code you wish you had
+  expect(current_path).to eq(new_user_registration_path)
 end
 
 And(/^I can see an Error Notification$/) do
-  pending # express the regexp above with the code you wish you had
+  expect(page).to have_content("errors")
 end
 
 And(/^I can see Sign In link$/) do
-  pending # express the regexp above with the code you wish you had
+  within(:css, "#nav"){ expect(page).to have_content("Sign In")}
 end
