b5e035f6190bea5350de13899a8809be7f8bcfe1
Laurie Williams <laurie.huw.williams@googlemail.com>
2015-06-15 19:16:12 +0100

    Refactor file_drop_upload.js

--- app/assets/javascripts/file_drop_upload.js
+++ app/assets/javascripts/file_drop_upload.js
@@ -18,19 +18,24 @@ var fileDropper = {
         $(this).removeClass("drag_over");
         fileDropper.upload(event);
     },
-    upload: function(event){
-        //Get File
+    setDropFile: function(event){
         var file = event.originalEvent.dataTransfer.files;
         file = file[0];
-        //Get CSRF Token
+        fileDropper.dropFile = file;
+    },
+    setCsrfToken: function(){
         var csrf_token = $('meta[name="csrf-token"]');
         if (csrf_token.length > 0) {csrf_token = csrf_token[0].content;}
+        fileDropper.csrfToken = csrf_token;
+    },
+    setFormData: function(file, csrf_token){
         //Set Form Data
         var requestData = new FormData();
         requestData.append("piece[image]", file);
         requestData.append("authenticity_token", csrf_token);
-
-        //Send Request
+        fileDropper.requestData = requestData;
+    },
+    sendAjaxRequest: function(requestData){
         $.ajax({
             processData: false,
             contentType: false,
@@ -38,10 +43,19 @@ var fileDropper = {
             url: "/pieces.json",
             data: requestData,
             success: function(data){
-            //    redirect to edit page
+                //    redirect to edit page
                 window.location.replace("/pieces/" + data.id + "/edit")
             }
         });
+    },
+    upload: function(event){
+        fileDropper.setDropFile(event);
+        var file = fileDropper.dropFile;
+        fileDropper.setCsrfToken();
+        var csrfToken = fileDropper.csrfToken;
+        fileDropper.setFormData(file, csrfToken);
+        var requestData = fileDropper.requestData;
+        fileDropper.sendAjaxRequest(requestData);
     }
 };
 
