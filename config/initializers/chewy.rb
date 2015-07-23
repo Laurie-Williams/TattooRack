a1f5fab6fec960f2ac5170bb238cbd2c0c6ca62b
Laurie Williams <laurie.huw.williams@googlemail.com>
2015-07-23 10:03:14 +0100

    Edit elasticsearch initializer to use yml file for multiple environments

--- config/initializers/chewy.rb
+++ config/initializers/chewy.rb
@@ -1,9 +1,9 @@
-if Rails.env.production?
-  Chewy.settings = {host: ENV['SEARCHBOX_URL']}
-  Chewy.settings = {prefix: Rails.env }
-else
-  Chewy.settings = {host: 'localhost:9250'}
-  Chewy.settings = {prefix: Rails.env }
-end
+# if Rails.env.production?
+#   Chewy.settings = {host: ENV['SEARCHBOX_URL']}
+#   Chewy.settings = {prefix: Rails.env }
+# else
+#   Chewy.settings = {host: 'localhost:9250'}
+#   Chewy.settings = {prefix: Rails.env }
+# end
 
 Chewy.use_after_commit_callbacks = !Rails.env.test?
